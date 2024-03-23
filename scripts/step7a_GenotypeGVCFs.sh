d42b11c37c0ea248d328cd2799b20a2ced828971
Ayasha Abdalla-Wyse <ayasha@beluga3.int.ets1.calculquebec.ca>
2024-03-23 16:27:05 -0400

    genotype first

--- scripts/step7a_GenotypeGVCFs.sh
+++ scripts/step7a_GenotypeGVCFs.sh
@@ -5,19 +5,19 @@
 #SBATCH --mem-per-cpu=64G              #This is the memory reserved per core.
 #Total memory reserved: 64GB
 
-#SBATCH --time=300:00:00        #This is the time that your task will run
-#SBATCH --qos=2weeks           #You will run in this queue
+#SBATCH --time=168:00:00        #This is the time that your task will run
+#SBATCH --qos=7days           #You will run in this queue
 
 # Paths to STDOUT or STDERR files should be absolute or relative to current working directory
-#SBATCH --output=/home/ayasha/scratch/logs/7aGenoGVCFstdout.txt     #These are the STDOUT and STDERR files
-#SBATCH --error=/home/ayasha/scratch/logs/7aGenoGVCFstderr.txt
+#SBATCH --output=/home/ayasha/scratch/logs/GenoGVCFstdout.txt     #These are the STDOUT and STDERR files
+#SBATCH --error=/home/ayasha/scratch/logs/GenoGVCFstderr.txt
 
 #You selected an array of jobs from 1 to 9 with 9 simultaneous jobs
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
 #SBATCH --mail-user=ayasha.abdallawyse@mail.utoronto.ca        #You will be notified via email when your task ends or fails
 
 #This job runs from the current working directory
-#SBATCH --workdir=/home/ayasha/scratch/temp_data/
+
 
 #Remember:
 #The variable $TMPDIR points to the local hard disks in the computing nodes.
@@ -29,6 +29,7 @@
 #################################
 
 module load java
+module load gatk
 
 #export your required environment variables below
 #################################################
@@ -43,5 +44,6 @@ module load java
 # # this is the second column of index_array_40x.csv
 # INTERVAL=`sed -n "$SLURM_ARRAY_TASK_ID"p "${file_list}" | cut -f 3 -d ','`
 
+cd ~/scratch/temp_data/
 gatk GenotypeGVCFs -R ~/projects/def-mshafer/genome/Oreochromis_niloticus.O_niloticus_UMD_NMBU.dna.toplevel.fa -V NMBU_cohort_final.g.vcf.gz -O NMBU_cohort_genotyped_whole.g.vcf.gz
 
